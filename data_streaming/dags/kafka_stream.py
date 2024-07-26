88792898297b9895d3866bb1fac838caac4217fa
sharma-mitneu <145163557+sharma-mitneu@users.noreply.github.com>
2024-07-26 13:57:30 -0400

    adding changes made in kafka_stream for getting data

--- data_streaming/dags/kafka_stream.py
+++ data_streaming/dags/kafka_stream.py
@@ -7,15 +7,41 @@ default_args = {
     'start_date': datetime(2024, 8, 3, 10)
 }
 
-def stream_data():
-    import json
+def get_data():
     import requests
 
     res = requests.get("https://randomuser.me/api/")
-    print(res.json())
+    res = res.json()
+    res = res['results'][0]
+
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['picture'] = res['picture']['medium']
+
+    # data we need to get from res -->
+    # gender, address --> {street number, street name, city, state, country}, postcode, email, username, dob,
+    # registered_date, phone
+
+
+def stream_data():
+    import json
+
+    print(json.dumps(res, indent=3))
 
 with DAG('user_automation',
          default_args = default_args,
+
          schedule_interval='@daily',
          catchup=False) as dag:
 
