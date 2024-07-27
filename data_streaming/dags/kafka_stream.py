67992b0d992fef3e7e702e533b84b0795c3642f2
sharma-mitneu <145163557+sharma-mitneu@users.noreply.github.com>
2024-07-27 14:42:50 -0400

    adding docker compose yaml and stream changes

--- data_streaming/dags/kafka_stream.py
+++ data_streaming/dags/kafka_stream.py
@@ -7,12 +7,15 @@ default_args = {
     'start_date': datetime(2024, 8, 3, 10)
 }
 
+
 def get_data():
     import requests
 
     res = requests.get("https://randomuser.me/api/")
     res = res.json()
     res = res['results'][0]
+
+    return res
 
 
 def format_data(res):
@@ -33,18 +36,21 @@ def format_data(res):
     # gender, address --> {street number, street name, city, state, country}, postcode, email, username, dob,
     # registered_date, phone
 
+    return data
+
 
 def stream_data():
     import json
-
+    res = get_data()
+    res = format_data(res)
     print(json.dumps(res, indent=3))
 
+
 with DAG('user_automation',
-         default_args = default_args,
+         default_args=default_args,
 
          schedule_interval='@daily',
          catchup=False) as dag:
-
     streaming_task = PythonOperator(
         task_id='stream_data_from_api',
         python_callable=stream_data
